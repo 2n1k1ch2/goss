6e6bc4ce54361d2c468b106a787d10a743ae2f76
nbaev <nikita_uray@mail.ru>
2025-08-26 18:41:08 +0300

    mod: .gitignore & gather.go

--- pkg/pprof/gather.go
+++ pkg/pprof/gather.go
@@ -11,12 +11,25 @@ type GoroutineDump struct {
 	Stacks    []string //
 }
 
+func get_stack() (int, []byte) {
+
+	buf := make([]byte, 1024*1024) // 1MB обычно достаточно
+
+	for {
+		n := runtime.Stack(buf, true) // all = true
+		if n < len(buf) {
+			return n, buf[:n]
+		}
+		// Увеличиваем если не хватило
+		buf = make([]byte, 2*len(buf))
+	}
+}
 func CaptureAll() *GoroutineDump {
-	buf := make([]byte, 1<<20) // 1 MB буфер
-	n := runtime.Stack(buf, true)
+
+	n, buf := get_stack()
 	raw := string(buf[:n])
 
-	stacks := strings.Split(raw, "\n\n")
+	stacks := strings.Split(raw, "\n")
 
 	return &GoroutineDump{
 		Timestamp: time.Now(),
